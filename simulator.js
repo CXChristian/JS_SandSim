c5f0d29e565864af766f5fb269b864da905fd0f4
Conrad Christian <cchristian5@my.bcit.ca>
2024-11-03 02:43:27 -0800

    Modify board to align in center

--- simulator.js
+++ simulator.js
@@ -2,14 +2,13 @@
 Program to simulate sand falling through a grid of cells.
 Used to be run on a div called "game" in the HTML file.
 */
-let BOARD_ROWS = 20;
-let BOARD_COLUMNS = 8;
-let CELL_SIZE = 20;
-let CELL_BORDER_SIZE = 1;
+const BOARD_ROWS = 20;
+const BOARD_COLUMNS = 8;
+const CELL_SIZE = 20;
+const CELL_BORDER_SIZE = 1;
 
 function run() {
-    let html = document.getElementById("game");
-
+    const html = document.getElementById("game");
     let board = [];
 
     // Create a game board
@@ -18,10 +17,7 @@ function run() {
 
         let row = document.createElement("div");
         row.style.display = "flex";
-        row.style.padding = "0px";
         row.style.width = `${CELL_SIZE * BOARD_COLUMNS + (CELL_BORDER_SIZE * BOARD_COLUMNS)}px`;
-        row.style.margin = "0px";
-        row.style.backgroundColor = "blue";
 
         for (var j = 0; j < BOARD_COLUMNS; j++) {
             board[i][j] = 0;
@@ -31,9 +27,7 @@ function run() {
             cell.style.width = `${CELL_SIZE}px`;
             cell.style.height = `${CELL_SIZE}px`;
             cell.style.display = "inline-block";
-            cell.style.padding = "0px";
-            cell.style.margin = "0px";
-            cell.style.backgroundColor = "red";
+
 
             // Append cell to the game div
             row.appendChild(cell);
